5ed8b051feaedc98235e9790cb71476f8633dc45
Amit Dey <apu76899@gmail.com>
2021-01-07 18:40:34 +0530

    dev-phase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Create from "./components/newPost"
 import View from "./components/viewPosts"
 import { GiPostStamp } from "react-icons/gi";
 import { FaRegPlusSquare } from "react-icons/fa";
-import './App.css';
 import './components/style.css'
 
 class App extends Component {
@@ -18,17 +17,17 @@ class App extends Component {
   render() {
     const {viewPost} = this.state
     return (
-      <div>
-        <div className="headbar">
-          Postsgram
-        </div>
-        <div className="post_section">
-          {viewPost?<View />:<Create />}
-        </div>
-        <div className="nav">
-          <div onClick={()=>this.setState({viewPost:false})}><FaRegPlusSquare /></div>
-          <div onClick={()=>this.setState({viewPost:true})}><GiPostStamp /></div>
-        </div>
+      <div className="App">
+          <div className="headbar">
+            Postsgram
+          </div>
+          <div className="post_section">
+            {viewPost?<View />:<Create />}
+          </div>
+          <div className="nav">
+            <div onClick={()=>this.setState({viewPost:false})}><FaRegPlusSquare /></div>
+            <div onClick={()=>this.setState({viewPost:true})}><GiPostStamp /></div>
+          </div>
       </div>
     )
   }
--- src/components/viewPosts.js
+++ src/components/viewPosts.js
@@ -1,11 +1,68 @@
-import React, { Component } from 'react'
+import React, { useState, Component } from 'react'
 import './style.css'
+/* Image Import */
+import post1 from "./images/post/post1.jpg"
+import profile1 from "./images/profile/profile1.jpg"
 
-export class viewPosts extends Component {
+function Post({profileImage, userName, timeStamp, caption, postImage, likes, shares, comments}) {
+    const {show, alterShow} = useState(false)
+    return (
+        <div className="post">
+            <div className="post_head">
+                <img src={profileImage} alt="profile" />
+                <div className="profile_info"><p>{userName}</p><p>{timeStamp}</p></div>
+            </div>
+            <div className="caption">{caption}</div>
+            <div className="post_image"><img src={postImage} alt="post" /></div>
+            <div className="reaction_bar"><span>{likes}</span><span>{shares}</span><span onClick={()=>alterShow(!show)}>{comments.length}</span></div>
+            <div className="comment_tray">{show?comments.map(comment=>(<Comment {...comment} key={comment} />)):null}</div>
+        </div>
+    )
+}
+
+/*
+{
+    profile:"..///",
+    username:"name"
+    str:"Very nice"
+}
+*/
+
+function Comment({profile, username, str}) {
+    return (
+        <div className="detail_comment">
+            <img src={profile} alt="profile" />
+            <div className="comment_info"><span>{username}</span><span>{str}</span></div>
+        </div>
+    )
+}
+
+
+class viewPosts extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+                data:[
+                    {
+                        profileImage:profile1,
+                        userName:"Papai Parua",
+                        timeStamp:"November 12, 2020 at 3:00 PM",
+                        caption:"What a charming evening...",
+                        postImage:post1,
+                        likes:221,
+                        shares:39,
+                        comments:[]
+                    }
+                ]
+        }
+    }
+    
     render() {
+        const { data } = this.state
         return (
             <div>
-                viewpost
+                {data.map(post=><Post {...post} key={post} />)}
             </div>
         )
     }
