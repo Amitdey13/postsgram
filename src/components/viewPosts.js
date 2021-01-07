2706df415e0aa234cfb628a5067f756639a2e50c
Amit Dey <apu76899@gmail.com>
2021-01-07 20:38:59 +0530

    dev-phase

--- src/components/viewPosts.js
+++ src/components/viewPosts.js
@@ -1,4 +1,9 @@
 import React, { useState, Component } from 'react'
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
+import { RiShareForwardLine } from "react-icons/ri";
+import { GoComment } from "react-icons/go";
+
 import './style.css'
 /* Image Import */
 import post1 from "./images/post/post1.jpg"
@@ -11,10 +16,12 @@ function Post({profileImage, userName, timeStamp, caption, postImage, likes, sha
             <div className="post_head">
                 <img src={profileImage} alt="profile" />
                 <div className="profile_info"><p>{userName}</p><p>{timeStamp}</p></div>
+                <div className="post_menu"><BiDotsHorizontalRounded /></div>
             </div>
             <div className="caption">{caption}</div>
             <div className="post_image"><img src={postImage} alt="post" /></div>
-            <div className="reaction_bar"><span>{likes}</span><span>{shares}</span><span onClick={()=>alterShow(!show)}>{comments.length}</span></div>
+            <div className="reaction_value_bar"><span><AiFillLike className="likeicon" />{likes}</span><span onClick={()=>alterShow(!show)}>{comments.length} Comments</span><span>{shares} Shares</span></div>
+            <div className="reaction_button"><span><AiOutlineLike className="icon" /> Like</span><span><GoComment className="icon" /> Comment</span><span><RiShareForwardLine className="icon" /> Share</span></div>
             <div className="comment_tray">{show?comments.map(comment=>(<Comment {...comment} key={comment} />)):null}</div>
         </div>
     )
@@ -52,7 +59,28 @@ class viewPosts extends Component {
                         postImage:post1,
                         likes:221,
                         shares:39,
-                        comments:[]
+                        comments:[
+                            {
+                                profile:"..///",
+                                username:"name",
+                                str:"Very nice"
+                            },
+                            {
+                                profile:"..///",
+                                username:"name",
+                                str:"Very nice"
+                            },
+                            {
+                                profile:"..///",
+                                username:"name",
+                                str:"Very nice"
+                            },
+                            {
+                                profile:"..///",
+                                username:"name",
+                                str:"Very nice"
+                            }
+                        ]
                     }
                 ]
         }
@@ -61,7 +89,7 @@ class viewPosts extends Component {
     render() {
         const { data } = this.state
         return (
-            <div>
+            <div className="sub_post">
                 {data.map(post=><Post {...post} key={post} />)}
             </div>
         )
